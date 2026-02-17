eb7b38cc1a93578218ab23829d7f6db8d8429cbf
pranaykargam <sunnykargam@gmail.com>
2026-02-17 11:51:15 +0530

    updated

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -43,6 +43,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
+
     constructor(address initialOwner) ERC20("DecentralizedStableCoin", "DSC") Ownable(initialOwner) {}
 
     function burn(uint256 _amount) public override onlyOwner {
